ce235fc96e064e61d0b1a7edf9309de62d17a623
Rahulrocky749 <rockyrahul410@gmail.com>
2022-04-11 22:22:14 +0530

    Folder Structure

---
+++ src/org/string/Index.java
@@ -0,0 +1,22 @@
+package org.string;
+
+public class Index {
+     public static void main(String[] args) {
+    	 String s1 ="Greens Technology";
+ 		int lasPos=s1.lastIndexOf("o");
+ 		System.out.println(lasPos);
+ 		
+ 		String s2 ="Greens Technology";
+ 		int las=s2.lastIndexOf("n");
+ 		System.out.println(las);
+ 		
+ 		String s3 ="velemurgan";
+ 		int lasP=s3.lastIndexOf("n");
+ 		System.out.println(lasP);
+ 		
+ 		String s4 ="j a v a p r o g a m m ";
+ 		int lasPo=s4.lastIndexOf(" ");
+ 		System.out.println(lasPo);
+	
+ }
+}
---
+++ src/org/string/Length.java
@@ -0,0 +1,28 @@
+package org.string;
+
+public class Length {
+
+
+	public static void main(String[] args) {
+		String s1 ="Greens Technology";
+		int length=s1.length();
+		System.out.println(length);
+		
+		String s2 ="Seleniumautomation";
+		int le=s2.length();
+		System.out.println(le);
+		
+		String s3 ="velmurgan";
+		int len=s3.length();
+		System.out.println(len);
+		
+		String s4 ="J a v a p r o g r a m m";
+		int leng=s4.length();
+		System.out.println(leng);
+		
+		String s5 ="9751070259";
+		int lengt=s5.length();
+		System.out.println(lengt);
+	}
+
+}
